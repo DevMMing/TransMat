24f2af3891475dd91a245711b76a687bfd4764cd
DevMMing <mming@stuy.edu>
2019-03-04 16:52:16 -0500

    DONE

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from parser import *
 from matrix import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [ 255, 255, 0 ]
 edges = []
 transform = new_matrix()
 
-parse_file( 'script', edges, transform, screen, color )
-parse_file('script2',edges,transform,screen,color)
+parse_file('script2', edges, transform, screen, color )
